4997544217d7a919c75e6ea4a8420734c7a33068
Roberto Markus <markusrobertoo@gmail.com>
2021-07-25 22:43:49 +0700

    Episode 7

---
+++ apps_anonymous_method/android/app/src/main/kotlin/com/example/apps_anonymous_method/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apps_anonymous_method
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ apps_anonymous_method/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String message = "This is the text";
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Anonymous Method"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(message),
+              RaisedButton(
+                child: Text("Click here"),
+                onPressed: () {
+                   setState(() {
+                    message = "Button has been pressed, so the text is changed";  
+                  });  
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
