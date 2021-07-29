8b4cfdaf4cf9e8695460ba7426e922a2e652717e
Roberto Markus <markusrobertoo@gmail.com>
2021-07-29 15:14:49 +0700

    Episode 16

---
+++ apps_navigate_multiscreen/android/app/src/main/kotlin/com/example/apps_navigate_multiscreen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apps_navigate_multiscreen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ apps_navigate_multiscreen/lib/main_page.dart
@@ -0,0 +1,22 @@
+import 'package:apps_navigate_multiscreen/second_page.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Go to Second Page"),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SecondPage();
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
